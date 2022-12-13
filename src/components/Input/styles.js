0ae8f29c95f54145eab1cdad79a3ffa9e061ee75
Thalessombra <thales.sombra@hotmail.com>
2022-12-13 13:57:23 -0300

    começo  do projeto

---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+width: 100%;
+height: 75px;
+background-color: #AAFFAA;
+font-size: 24px;
+font-family: 'Roboto';
+
+Input {
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0;
+    font-size: 24px;
+    font-family: 'Roboto';
+    color: #FFFFFF;
+}
+`
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-color: #CACACA;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const Content = styled.div`
+width: 50%;
+background-color: #FAFAFA;
+align-items: center;
+justify-content: center;
+
+`
+export const Row = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+`
+export const Column = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+`
